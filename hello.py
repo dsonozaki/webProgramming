5fc628e725b0ef0ffca2ae1f5de3a616e5ac6bf9
dmaterialist <turbocat14@gmail.com>
2023-07-03 20:52:20 +0300

    update

---
+++ hello.py
@@ -0,0 +1,6 @@
+def hello(environ,start_response):
+  status = '200 OK'
+  headers = [ ("Content-Type","text/plain")]
+  body = environ["QUERY_STRING"].replace("&","\n")
+  start_response(status,headers)
+  return [body]
